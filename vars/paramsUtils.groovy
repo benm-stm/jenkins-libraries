fa749b3aa8143f86c62a0c25e182ed553c7a4a91
benm-stm <benm.stm@gmail.com>
2018-09-17 17:25:49 +0200

    add multiline param to libraries

--- vars/paramsUtils.groovy
+++ vars/paramsUtils.groovy
@@ -1,3 +1,5 @@
+import com.cwctravel.hudson.plugins.extended_choice_parameter.ExtendedChoiceParameterDefinition
+
 def multiSelect(list, list_name, project_name) {
 	def multiSelect = new ExtendedChoiceParameterDefinition(list_name, 
 			"PT_MULTI_SELECT", 
