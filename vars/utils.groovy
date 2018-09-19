795dc154bb108e2d6f99da2b7297fc1c4dbc0ef5
benm-stm <benm.stm@gmail.com>
2018-09-19 11:09:57 +0200

    tweaks

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -21,7 +21,7 @@ def key_value_parser(file, key_param) {
 
 def getFileNamesListUnderPath(path, filters, remove_extention=false) {
     filters += " | sed -e 's@/@@g'"
-    if ( remove_extension )
+    if ( remove_extention )
         filters += " | sed 's/\\.[^.]*\$//'"
     list = sh (
         script: "cd ${path} && ls | grep ${filters}",
